ab11e14cc43d80308fcb7c266d2de86c70b2f574
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-05-16 16:29:56 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,8 @@
+let subjects = ['Python', 'C++', 'VB.Net', 'Javascript', 'Assembly'];
+let subject = subjects.at(4);
+console.log(subject)
+document.write(`<h3>${subject}</h3> <br> <p>The above is the last item within my array and below is the original array</p> <br> <h1 style=color:blue>${subjects}</h1>`)
+
+let middle = subjects.at(2);
+console.log(middle)
+document.write(middle)
